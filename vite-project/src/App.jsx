a678450f87da2ea13f6610d0727b9858bbe51b0c
alekspanteli <alekspanteli@rambler.ru>
2022-10-11 07:53:58 +0000

    Update App.jsx and tailwind.config.cjs

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="container mx-auto px-4">
+    <div className="container">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
