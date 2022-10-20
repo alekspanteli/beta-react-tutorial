621e9d8e10fc79ce8a17443bd4cbe646bc604ece
alekspanteli <alekspanteli@rambler.ru>
2022-10-20 07:51:11 +0000

    Update 3 files

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,10 @@
 // import Gallery, { Profile } from "./Gallery";
-import ProfileProps from "./ProfileProps";
+import Avatar from "./Avatar";
 import "./App.css";
 function App() {
   return (
     <div className="container">
-      <ProfileProps
-        person={{ name: "Alex Panteli", imageId: "100" }}
-        size={100}
-      />
-      <ProfileProps
-        person={{ name: "Alex Panteli", imageId: "200" }}
-        size={200}
-      />
+      <Avatar />
     </div>
   );
 }
--- vite-project/src/Avatar.jsx
+++ vite-project/src/Avatar.jsx
@@ -1,10 +1,24 @@
+// export default function Avatar() {
+//   const avatar = "https://via.placeholder.com/100";
+//   const desc = "Alex Panteli";
+//   return (
+//     <>
+//       <h1 style={{ backgroundColor: "red" }}>Hello {desc}</h1>
+//       <img className="avatar" src={avatar} alt={desc} />
+//     </>
+//   );
+// }
+
+import Card from "./Card";
+
 export default function Avatar() {
-  const avatar = "https://via.placeholder.com/100";
-  const desc = "Alex Panteli";
   return (
-    <>
-      <h1 style={{ backgroundColor: "red" }}>Hello {desc}</h1>
-      <img className="avatar" src={avatar} alt={desc} />
-    </>
+    <Card>
+      <img
+        className="avatar"
+        src="https://via.placeholder.com/100"
+        alt="Alex Panteli"
+      />
+    </Card>
   );
 }
